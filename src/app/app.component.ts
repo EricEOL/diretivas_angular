5cc1b1e5a918d6acad06da54cdb5c37e601816ae
EricEOL <lopes.eric051@gmail.com>
2022-04-11 12:48:41 -0300

    ngSwitch e ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Person {
+  name: string;
+  lastName: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,6 +13,21 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   
   count: number = 0;
+
+  people: Person[] = [
+    {
+      name: "Eric",
+      lastName: "Lopes"
+    },
+    {
+      name: "Panda",
+      lastName: "Silva"
+    },
+    {
+      name: "Texugo",
+      lastName: "Oliveira"
+    }
+  ]
 
   constructor() {}
 
